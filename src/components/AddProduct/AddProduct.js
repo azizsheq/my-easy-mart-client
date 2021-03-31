f724efd8ff20f2de9c401236d303cc2d763445c6
azizsheq <azizsheq@gmail.com>
2021-04-01 07:17:13 +0800

    add product form complete with react bootstrap

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,25 +1,57 @@
 import React from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 import './AddProduct.css'
 
 const AddProduct = () => {
     return (
         <div className="addProductForm">
-            <form action="">
-                <label htmlFor="name">Product Name</label>
-                <input id="name" type="text"/>
+            <div className="container">
+                <Form>
+                    <Form.Group as={Row} controlId="formHorizontalName">
+                        <Form.Label column sm={4}>
+                            Product Name
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control type="text" placeholder="Enter Name" />
+                        </Col>
+                    </Form.Group>
 
-                <label htmlFor="price">Unite Price</label>
-                <input id="price" type="text"/>
+                    <Form.Group as={Row} controlId="formHorizontalDescription">
+                        <Form.Label column sm={4}>
+                            Description
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control type="text" placeholder="Enter Description" />
+                        </Col>
+                    </Form.Group>
 
-                <label htmlFor="description">Description</label>
-                <input id="description" type="text"/>
+                    <Form.Group as={Row} controlId="formHorizontalPrice">
+                        <Form.Label column sm={4}>
+                            Price
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control type="text" placeholder="Enter Unite Price" />
+                        </Col>
+                    </Form.Group>
 
-                <label htmlFor="imageFile">Add Photo</label>
-                <input id="imageFile" type="file"/>
+                    <Form.Group as={Row} controlId="formHorizontalFile">
+                        <Form.Label column sm={4}>
+                            Add Photo
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control type="file"/>
+                        </Col>
+                    </Form.Group>
 
-                <button>Submit</button>
-            </form>
-    </div>
+                    <Form.Group as={Row}>
+                        <Col sm={8}></Col>
+                        <Col sm={4}>
+                            <Button variant="primary" size="lg" block type="submit">Save</Button>
+                        </Col>
+                    </Form.Group>
+                </Form>
+            </div>
+        </div>
     );
 };
 
--- src/components/Deals/Deals.js
+++ src/components/Deals/Deals.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button, Spinner } from 'react-bootstrap';
 
 const Deals = () => {
     return (
         <div>
             <h1>DEALS</h1>
+            <h2>Spinner</h2>
+            <Button>Button</Button>
+            <Spinner animation="border" variant="success" />
         </div>
     );
 };
